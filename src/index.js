3fad8a990c05d8570e078eba9fa03864ee02fe59
Hristijan <hmalezanoski@gmail.com>
2020-05-29 10:02:40 +0800

    add children component

--- src/index.js
+++ src/index.js
@@ -6,28 +6,41 @@ import ApprovalCard from "./AppruvalCard";
 
 const App = () => {
 
-    return(
+    return (
         <div className="ui container comments">
-            <ApprovalCard />
-           <CommentDetail autor="Sam"
-                          avatarImage={faker.image.avatar()}
-                          timeAgo="Today at 04:00 PM"
-                          commentText="Gigi is the best developer"
-           />
-           <CommentDetail autor="Jake"
-                          avatarImage={faker.image.avatar()}
-                          timeAgo="Today at 02:00 AM"
-                          commentText="React is best library"
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to do this
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail autor="Sam"
+                               avatarImage={faker.image.avatar()}
+                               timeAgo="Today at 04:00 PM"
+                               commentText="Gigi is the best developer"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail autor="Jake"
+                               avatarImage={faker.image.avatar()}
+                               timeAgo="Today at 02:00 AM"
+                               commentText="React is best library"
 
-           />
-           <CommentDetail autor="Gigi"
-                          avatarImage={faker.image.avatar()}
-                          timeAgo="Yesterday at 02:40 PM"
-                          commentText="I want to go home"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail autor="Gigi"
+                               avatarImage={faker.image.avatar()}
+                               timeAgo="Yesterday at 02:40 PM"
+                               commentText="I want to go home"
 
-           />
+                />
+            </ApprovalCard>
+
+
         </div>
     )
 }
 
-ReactDOM.render(<App />,document.querySelector('#root'))
+ReactDOM.render(<App/>, document.querySelector('#root'))
